cce6845370f5bfd31892fe187c9f991fbe985eba
Ramon Carmenaty <rccarmenaty@icloud.com>
2021-04-15 11:10:03 -0400

    changing folder name

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 
-app.listen(4000, () => console.log('up adn running'))
+app.listen(4000, () => console.log('up and asd running on 4002 '))
